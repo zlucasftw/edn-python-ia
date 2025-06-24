6d0d2dea8ad666dbcc9842776eea1f6dbf03135c
zlucasftw <ilucasmcpvp@gmail.com>
2025-06-24 11:07:35 -0300

    add: exemplo declaracao e atribuicao

---
+++ exemplos/exemplo.py
@@ -0,0 +1,11 @@
+nome = "Aline"  # String
+# idade = 30  # Inteiro
+idade, altura = 30, 1.65  # Inteiro, Float
+# altura = 1.65   # Float
+contador = 0
+contador += 1
+contador += 1
+# contador += 5
+contador = 5
+
+print(contador)
