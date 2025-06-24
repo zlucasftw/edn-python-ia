7d781c003856b7ed390bafe8f35f8e38b50972cd
zlucasftw <ilucasmcpvp@gmail.com>
2025-06-24 11:33:59 -0300

    add: exemplo - operadores de comparacao

--- exemplos/exemplo.py
+++ exemplos/exemplo.py
@@ -1,20 +1,13 @@
-"""
-Adição (+): 5 + 3 = 8
-Subtração (-): 5 - 3 = 2
-Multiplicação (*): 5 * 3 = 15
-Exponenciação (**): 5 ** 3 = 125
-Divisão (/): 5 / 3 = 1.6666666666666667
-Divisão Inteira (//): = 5 // 3 = 1
-Módulo (%): 5 % 3 = 2
-"""
+x = 10
+# y = 15
+# y = 10
+# y = 5
+y = 10
 
 
-
-# Divisao = 5 / 3
-# Divisao = 5 // 3
-# Divisao = 5 % 3
-# Divisao = 17 // 2
-# print(Divisao)
-
-Exponenciacao = 5 ** 3
-print(Exponenciacao)
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x <= y)
+print(x <= y)
