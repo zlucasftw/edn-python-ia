db3d89288b39af9ca1f77435422982197b71a9ae
zlucasftw <ilucasmcpvp@gmail.com>
2025-06-24 11:28:19 -0300

    add: exemplo - operacoes aritmeticas

--- exemplos/exemplo.py
+++ exemplos/exemplo.py
@@ -1,5 +1,20 @@
-x = 5   # x é
+"""
+Adição (+): 5 + 3 = 8
+Subtração (-): 5 - 3 = 2
+Multiplicação (*): 5 * 3 = 15
+Exponenciação (**): 5 ** 3 = 125
+Divisão (/): 5 / 3 = 1.6666666666666667
+Divisão Inteira (//): = 5 // 3 = 1
+Módulo (%): 5 % 3 = 2
+"""
 
-x = "hello" # x é string
 
-x = 3.14    # é float
+
+# Divisao = 5 / 3
+# Divisao = 5 // 3
+# Divisao = 5 % 3
+# Divisao = 17 // 2
+# print(Divisao)
+
+Exponenciacao = 5 ** 3
+print(Exponenciacao)
