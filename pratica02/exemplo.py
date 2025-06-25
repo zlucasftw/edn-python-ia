7a7e1334f86d14c51e13296e789a38119ed37c65
zlucasftw <ilucasmcpvp@gmail.com>
2025-06-25 11:38:02 -0300

    add: exemplo da funcao print e formatacao de strings

--- pratica02/exemplo.py
+++ pratica02/exemplo.py
@@ -1,6 +1,7 @@
-idade = input("Digite a idade: ")
-altura = input("Digite a altura: ")
-idade = int(idade)
-altura = float(altura)
-print(idade)
-print(altura)
+nome = "Lucas"
+idade = 25
+print("Meu nome é " + nome + " e eu tenho " + str(idade) + " anos.")
+
+print(f"Meu nome é {nome} e eu tenho {idade} anos.")    # f-strings
+
+print("Meu nome é {} e eu tenho {} anos.".format(nome, idade))
