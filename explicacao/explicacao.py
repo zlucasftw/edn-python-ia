c65a68fa7aeda15c1f6b03504973a4af970b7716
zlucasftw <ilucasmcpvp@gmail.com>
2025-07-03 09:42:15 -0300

    add: explicacao - escrita arquivos csv

--- explicacao/explicacao.py
+++ explicacao/explicacao.py
@@ -1,6 +1,6 @@
 import csv
-with open("dados.csv", "r") as arquivo_csv:
-    leitor = csv.reader(arquivo_csv)
-    for linha in leitor:
-        print(linha)
+with open("dados.csv", "w") as arquivo_csv:
+    escritor = csv.writer(arquivo_csv)
+    escritor.writerow(['nome','idade','cidade'])
+    escritor.writerow(['Maria','30','Salvador'])
 
