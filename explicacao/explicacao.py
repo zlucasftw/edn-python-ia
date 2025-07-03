ffc0e58f5a02e3d5a4b4b8b65b89e2a2bf68bcd5
zlucasftw <ilucasmcpvp@gmail.com>
2025-07-03 09:39:54 -0300

    add: explicacao - leitura arquivos csv

--- explicacao/explicacao.py
+++ explicacao/explicacao.py
@@ -1,6 +1,6 @@
-# numero = int(input("Digite um número: "))
-# quadrado = lambda x : x ** 2
-# print(f"O quadrado de {numero} é {quadrado(numero)}")
-
-with open("/caminho_do_arquivo")
+import csv
+with open("dados.csv", "r") as arquivo_csv:
+    leitor = csv.reader(arquivo_csv)
+    for linha in leitor:
+        print(linha)
 
